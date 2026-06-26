a619f8e3be3aef038224bda6ac276b4d2f599ffa
SomeRookieDev49 <anudari.enkhbat049@gmail.com>
2026-06-26 12:23:29 +0800

    s

--- web/src/lib/backend-api.ts
+++ web/src/lib/backend-api.ts
@@ -1,7 +1,7 @@
 import { firebaseAuth } from "@/lib/firebase";
 
 const API_BASE_URL = (
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://127.0.0.1:8000"
+  process.env.NEXT_PUBLIC_API_BASE_URL!
 ).replace(/\/+$/, "");
 
 // Нэвтэрсэн хэрэглэгчийн Firebase ID token-ийг бүх backend хүсэлтэд автоматаар хавсаргадаг
